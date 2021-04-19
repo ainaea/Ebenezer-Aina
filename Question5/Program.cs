4c062bdfab04e3c31d9c2bc1ecf28a0775dec744
ainaea <ebenezer.a.aina@gmail.com>
2021-04-19 16:05:30 +0100

    Submission

---
+++ Question5/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Question5
+{
+    class Program
+    {
+        enum Months {January=1, February, March, April, May, June, July, August, September, October, November, December}
+        static void Main(string[] args)
+        {
+            // Console.WriteLine("Hello World!");
+            while (true)
+            {
+                Console.Write("Enter a number between 1-12 to play and any other number to quit: ");
+                int num = int.Parse( Console.ReadLine());
+
+                if (num < 1 || num > 12)
+                {
+                    Console.Write("Good bye");
+                    break;
+                }
+            
+                Console.WriteLine((Months) num);
+            }
+        }
+    }
+}
